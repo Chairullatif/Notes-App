4d5574c8143174cfcf5ee02e44993943f93013e8
Chairullatif Aji Sadewa <khoirullatif337@gmail.com>
2021-08-11 09:10:51 +0700

    menambahkan kebutuhan DML (CRUD)

---
+++ app/src/main/java/com/khoirullatif/notes/database/NoteHelper.kt
@@ -0,0 +1,83 @@
+package com.khoirullatif.notes.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import com.khoirullatif.notes.database.DatabaseContract.NoteColumns.Companion.TABLE_NAME
+import com.khoirullatif.notes.database.DatabaseContract.NoteColumns.Companion._ID
+import java.sql.SQLException
+
+class NoteHelper (context: Context) {
+    private var databaseHelper: DatabaseHelper = DatabaseHelper(context)
+    private lateinit var database: SQLiteDatabase
+
+    companion object {
+        private const val DATABASE_TABLE = TABLE_NAME
+
+        // Nantinya digunakan untuk inisiasi database
+        private var INSTANCE: NoteHelper? = null
+        fun getInstance(context: Context): NoteHelper =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: NoteHelper(context)
+            }
+    }
+
+    // Membuka dan menutup koneksi database
+    @Throws(SQLException::class)
+    fun open() {
+        database = databaseHelper.writableDatabase
+    }
+
+    fun close() {
+        databaseHelper.close()
+
+        if (database.isOpen) {
+            database.close()
+        }
+    }
+
+    //      Metode CRUD
+    // 1. read all data
+    fun queryAll(): Cursor {
+        return database.query(
+            DATABASE_TABLE,
+            null,
+            null,
+            null,
+            null,
+            null,
+            "$_ID ASC"
+        )
+    }
+
+    //2. read data with id
+    fun queryById(id: String): Cursor {
+        return database.query(
+            DATABASE_TABLE,
+            null,
+            "$_ID = ?",
+            arrayOf(id),
+            null,
+            null,
+            null
+        )
+    }
+
+    //3. insert data
+    fun insert(values: ContentValues): Long {
+        return database.insert(DATABASE_TABLE, null, values)
+    }
+
+    //4. update data
+    fun update(id: String, values: ContentValues): Int {
+        return database.update(DATABASE_TABLE, values, "$_ID = ?", arrayOf(id))
+    }
+
+    //5. delete data
+    fun deleteById(id: String): Int {
+        return database.delete(DATABASE_TABLE, "$_ID = '$id'", null)
+    }
+
+    // Question is, what is difference betwen "... = ?" and "... LIKE ?"
+}
